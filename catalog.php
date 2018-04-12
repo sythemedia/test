b341eab0050bc8c9f3a3d915c20113d6aa98f1a0
sythemedia <36555749+sythemedia@users.noreply.github.com>
2018-04-11 20:01:34 -0400

    Create catalog.php

---
+++ catalog.php
@@ -0,0 +1,99 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" id="pee">
+<head>
+<title>RBLXTHUNDER</title>
+<link rel="stylesheet" type="text/css" href="style.css" title="normal"/>
+<link rel="Shortcut Icon" type="image/ico" href="images/WinServer.ico"/>
+<script type="text/javascript" src="js/js_funcs.js"></script>
+</head>
+<body onload="set_style_from_cookie()">
+<div id="Container">
+				<div id="Header">
+					<div id="Banner">
+						<center><div id="Logo"><a id="logo" title="RBLX" href="index.php" style="display:inline-block;cursor:pointer;"><img src="images/6DCCBF27-4C5D-485D-8D03-BB5D815D078A.png" border="0" id="img" alt="Origins06"/></a></div></center>
+					</div>
+					<div class="Navigation">
+						<span><a id="Servers" class="MenuItem" href="servers.php">Servers</a></span>
+						
+ 					</div>
+				</div>
+				<div id="Body">
+					
+	<div id="SplashContainer">
+		<div id="MainPanel">
+			<center>
+      <a href="https://rbxanus.ml/catalog/create.php">New Item</a>
+<h3>Categories</h3>
+<ul>
+<li><a href="https://rbxanus.ml/catalog/?cat=0">Hats</a></li>
+<li><a href="https://rbxanus.ml/catalog/?cat=1">Shirts</a></li>
+<li><a href="https://rbxanus.ml/catalog/?cat=2">T-Shirts</a></li>
+<li><a href="https://rbxanus.ml/catalog/?cat=3">Pants</a></li>
+<li><a href="https://rbxanus.ml/catalog/?cat=4">Faces</a></li>
+<li><a href="https://rbxanus.ml/catalog/?cat=5">Packages</a></li>
+</ul>
+<h3>RBXANUS Hats</h3>
+</div>
+<div>
+<div class="box">
+<img src="https://t5.rbxcdn.com/8ee196346f24a9e2cc509c063b0a858f" alt="Thumbnail" style="width:120px;height:120px;border:1px solid;">
+<p><a href="https://rbxanus.ml/catalog/view.php?id=78">Pirate Captain's Hat</a> by <a href="https://rbxanus.ml/profile.php?id=1">coke</a></p>
+<p><b>Price: </b>20 anuses</p>
+</div>
+<div class="box">
+<img src="https://t7.rbxcdn.com/c8a6007909d13d79b9ba20772bb10c41" alt="Thumbnail" style="width:120px;height:120px;border:1px solid;">
+<p><a href="https://rbxanus.ml/catalog/view.php?id=69">Bucket</a> by <a href="https://rbxanus.ml/profile.php?id=25">Smiley</a></p>
+<p><b>Price: </b>5 anuses</p>
+</div>
+<div class="box">
+<img src="https://t1.rbxcdn.com/d3f94bbcb9be71215574f44051effc78" alt="Thumbnail" style="width:120px;height:120px;border:1px solid;">
+<p><a href="https://rbxanus.ml/catalog/view.php?id=68">Traffic Cone</a> by <a href="https://rbxanus.ml/profile.php?id=25">Smiley</a></p>
+<p><b>Price: </b>25 anuses</p>
+</div>
+<div class="box">
+<img src="https://t1.rbxcdn.com/e14213d467d22f6c192239d2536764de" alt="Thumbnail" style="width:120px;height:120px;border:1px solid;">
+<p><a href="https://rbxanus.ml/catalog/view.php?id=67">Red Roblox Cap</a> by <a href="https://rbxanus.ml/profile.php?id=25">Smiley</a></p>
+<p><b>Price: </b>0 anuses</p>
+</div>
+<div class="box">
+<img src="https://t0.rbxcdn.com/5c0de7ff8e1406f15c85f2f0deaef96c" alt="Thumbnail" style="width:120px;height:120px;border:1px solid;">
+<p><a href="https://rbxanus.ml/catalog/view.php?id=66">Goofy Gordon</a> by <a href="https://rbxanus.ml/profile.php?id=16">City</a></p>
+<p><b>Price: </b>35 anuses</p>
+</div>
+<div class="box">
+<img src="https://t7.rbxcdn.com/e66ad4cdb354107a39b1a4cacea93034" alt="Thumbnail" style="width:120px;height:120px;border:1px solid;">
+<p><a href="https://rbxanus.ml/catalog/view.php?id=55">Radical Orange Helmet</a> by <a href="https://rbxanus.ml/profile.php?id=16">City</a></p>
+<p><b>Price: </b>1 anuses</p>
+</div>
+<div class="box">
+<img src="https://t6.rbxcdn.com/0efdb761654d4efa195977e6f40965e3" alt="Thumbnail" style="width:120px;height:120px;border:1px solid;">
+<p><a href="https://rbxanus.ml/catalog/view.php?id=49">Crown of the Dark Lord of SQL</a> by <a href="https://rbxanus.ml/profile.php?id=1">coke</a></p>
+<p><b>Price: </b>2,147,483,647 anuses</p>
+</div>
+<div class="box">
+<img src="https://t1.rbxcdn.com/bf19667abd9104ce6f23bfbfb4f466fc" alt="Thumbnail" style="width:120px;height:120px;border:1px solid;">
+<p><a href="https://rbxanus.ml/catalog/view.php?id=48">The Last Egg</a> by <a href="https://rbxanus.ml/profile.php?id=1">coke</a></p>
+<p><b>Price: </b>10 anuses</p>
+</div>
+<div class="box">
+<img src="https://t3.rbxcdn.com/4230d2d4deb54a2c3b74d9603157b13d" alt="Thumbnail" style="width:120px;height:120px;border:1px solid;">
+<p><a href="https://rbxanus.ml/catalog/view.php?id=47">The Masked Popcorn Enthusiast!</a> by <a href="https://rbxanus.ml/profile.php?id=1">coke</a></p>
+<p><b>Price: </b>25 anuses</p>
+</div>
+<div style="clear:both;"></div>
+<a>Prev</a>
+<a href="https://rbxanus.ml/catalog/?cat=0&page=1">1</a>
+<a href="https://rbxanus.ml/catalog/?cat=0&page=2">2</a>
+<a href="https://rbxanus.ml/catalog/?cat=0&page=3">3</a>
+<a href="https://rbxanus.ml/catalog/?cat=0&page=4">Next</a>
+<a href="https://rbxanus.ml/catalog/?cat=0&page=4">Last</a>
+</div>
+			</center>
+		</div>
+	</div>
+	</div>
+	
+			</div>
+	</body>
+</html>
